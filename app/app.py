d3000026b45fff613907f13dec7676405c89922f
Nebeyou Musie <Nebeyoumusie@gmail.com>
2024-07-22 10:59:34 +0000

    update file

---
+++ app/app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from utils import get_gemini_response, input_image_setup
+
+st.set_page_config(page_title="Gemini Health App")
+
+st.header("Gemini Health App")
+user_input = st.text_input("Input Prompt: ", key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image = ""   
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+submit = st.button("Tell me the total calories")
+
+input_prompt = """
+You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+"""
+
+# Debugging statement to check if the submit button was clicked
+print(f"Submit button clicked: {submit}")
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    print(f"Image data: {image_data}")  # Debugging statement to check the image data
+    
+    response = get_gemini_response(input_prompt, image_data, user_input)
+    print(f"Response: {response}")  # Debugging statement to check the response
+    
+    st.subheader("The Response is")
+    st.write(response)
--- app/utils.py
+++ app/utils.py
@@ -6,26 +6,23 @@ load_config()
 genai.configure(api_key=get_google_api_key())
 
 # Function to load Google Gemini Pro Vision API and get response
-def get_gemini_response(input, image, prompt):
-    model = genai.GenerationConfig('gemini-pro-vision')
-    response = model.generate_content([input, image[0], prompt])
+def get_gemini_repsonse(input,image,prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,image[0],prompt])
     return response.text
 
-# function to setup image input
-def input_image_setup(uploaded_image):
-    # check if a file has been uploaded
-    if uploaded_image is not None:
-        # read the file into bytes
-        bytes_data = uploaded_image.getvalue()
-        
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
         image_parts = [
             {
-                "mime_type": uploaded_image.type,
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
                 "data": bytes_data
             }
         ]
         return image_parts
     else:
-        raise FileNotFoundError("no file uploaded")
-     
-
+        raise FileNotFoundError("No file uploaded")
